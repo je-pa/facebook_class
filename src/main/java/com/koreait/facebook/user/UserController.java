1888e36e8aef872bf410c25ff30f12e9e5c5c484
kwonjaeeun <dino218@naver.com>
2021-06-21 17:03:05 +0900

    Initial commit

---
+++ src/main/java/com/koreait/facebook/user/UserController.java
@@ -0,0 +1,28 @@
+package com.koreait.facebook.user;
+
+import com.koreait.facebook.user.model.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/login")
+    public void login(){}
+
+    @GetMapping("/join")
+    public void join(UserEntity userEntity){}
+
+    @PostMapping("/join")
+    public String joinProc(UserEntity userEntity){
+        service.join(userEntity);
+        return "redirect:/feed/home";
+    }
+}
